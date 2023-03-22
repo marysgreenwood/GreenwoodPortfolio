c1e0670434b70d88a3ec5a232bd6090fac15f04a
marysgreenwood <marysgreenwood@gmail.com>
2023-03-22 18:19:15 -0400

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
-export default function Contact() {
-    return (
-<div>
-<h1>This is the contact page</h1>
-</div>
-    );
-}
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  const submit = () => {
+    if (name && email && message) {
+      const serviceId = "service_7dmb6sr";
+      const templateId = "template_bnajgof";
+      const publicKey = "Sh2-psiqmm5aj_rzm";
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      emailjs
+        .send(serviceId, templateId, templateParams, publicKey)
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
+
+      setName("");
+      setEmail("");
+      setMessage("");
+      setEmailSent(true);
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
+  return (
+    <div id="contact-form">
+      <input
+        type="text"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Your email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        placeholder="Your message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button onClick={submit}>Send Message</button>
+      <span className={emailSent ? "visible" : null}>
+        Thank you for your message, we will be in touch in no time!
+      </span>
+    </div>
+  );
+};
+
+export default Contact;
