634efbd425cc70fc0ce3ca88d3eef9264852c6c5
marysgreenwood <marysgreenwood@gmail.com>
2023-03-22 17:33:37 -0400

    added aboutMe

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import profilePic from "../assets/Profile Pic.jpeg";
 
 export default function AboutMe() {
-    return (
-<div>
-    <h1>This is the about me page</h1>
-</div>
-    );
+  return (
+    <div>
+      <img src={profilePic} />
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
 }
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../assets/Screenshot 2023-03-22 at 5.04.48 PM.png";
+import resume from "../assets/resume_screenshot.png";
 
 export default function Resume() {
   return (
