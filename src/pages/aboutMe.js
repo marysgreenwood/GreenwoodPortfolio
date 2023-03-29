65f06a508ae707f20b79cebc772a985b8dc991f0
marysgreenwood <marysgreenwood@gmail.com>
2023-03-29 18:09:43 -0400

    added text to about me

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -18,13 +18,8 @@ export default function AboutMe() {
       <div class="col-md-7" style={styles.bodyStyle}>
         <h2>About Me</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Junior software developer with background in academia. Excellent
+          organizational, analytical problem-solving and research skills.
         </p>
       </div>
     </div>
